0c7b1003f5312aff81928c18a37feabd7befd1c1
Xyron Uy <xyronuysoftware@gmail.com>
2025-01-15 22:59:04 +0800

    removed white bg in banner

--- src/components/home/Banner.jsx
+++ src/components/home/Banner.jsx
@@ -5,7 +5,7 @@ import React from "react";
 const Banner = () => {
     return (
         <div id="home" className="container h-[100vh] m-auto flex justify-center items-center">
-            <div className="bg-white bg-opacity-50 rounded-2xl text-black flex flex-col items-center justify-center w-full py-6 lg:p-20">
+            <div className="rounded-2xl text-black flex flex-col items-center justify-center w-full py-6 lg:p-20">
                 <h1 className="text-6xl text-center lg:text-7xl xl:text-9xl leading-none tracking-wide font-title font-medium">Your path<br/>to<br/>Virtual Assistant</h1>
                 <div className="py-5 w-[90%] lg:w-[60%] xl:w-[50%] 2xl:w-[40%] font-detail text-center font-normal text-sm md:text-base lg:text-lg">
                     <p>Your journey to becoming a successful Virtual Assistant is more than just a career move, it's a path to freedom, growth, and fulfillment. It's about stepping into a role that allows you to work on your own terms while making a meaningful impact for businesses around the world.</p>
